59b8577f3d92917cad9d908d5d936f03b58af4f3
KwangHo Cho <fishcopy6702@gmail.com>
2023-08-17 16:02:59 +0900

    Segger

---
+++ 001Tasks/Debug/ThirdParty/FreeRTOS/stream_buffer.d
@@ -0,0 +1,36 @@
+ThirdParty/FreeRTOS/stream_buffer.o: \
+ ../ThirdParty/FreeRTOS/stream_buffer.c \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/Global.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/stream_buffer.h
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/Global.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/stream_buffer.h:
---
+++ 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
