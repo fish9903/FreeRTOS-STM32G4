659de5df18f54447427b0c4046221528b4885a7b
KwangHo Cho <fishcopy6702@gmail.com>
2023-08-28 22:14:29 +0900

    add LED_Tasks2

---
+++ LED_Tasks2/Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c 
+
+OBJS += \
+./ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o 
+
+C_DEPS += \
+./ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/%.o ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/%.su ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/%.cyclo: ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/%.c ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G474xx -c -I../Core/Inc -I"C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/Config" -I"C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/OS" -I"C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/SEGGER" -I"C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/FreeRTOS" -I"C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/FreeRTOS/include" -I"C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-ThirdParty-2f-FreeRTOS-2f-portable-2f-GCC-2f-ARM_CM4F
+
+clean-ThirdParty-2f-FreeRTOS-2f-portable-2f-GCC-2f-ARM_CM4F:
+	-$(RM) ./ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.cyclo ./ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d ./ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o ./ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.su
+
+.PHONY: clean-ThirdParty-2f-FreeRTOS-2f-portable-2f-GCC-2f-ARM_CM4F
+
---
+++ LED_Tasks2/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/LED_Tasks2/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
