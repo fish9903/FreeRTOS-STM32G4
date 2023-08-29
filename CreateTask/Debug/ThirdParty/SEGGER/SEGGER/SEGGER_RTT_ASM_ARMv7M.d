7b64e7fe25788f5b8842222b6b5be47044646884
KwangHo Cho <fishcopy6702@gmail.com>
2023-08-29 23:07:58 +0900

    delete cache(.gitignore)

--- CreateTask/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/CreateTask/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/Users/fishc/Desktop/RTOS/Workspace/RTOS_workspace/CreateTask/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
